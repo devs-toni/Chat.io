e3377972c1fc7817359d7320356814fca77d3ce9
Antonio Rufino <devs.toni@gmail.com>
2023-02-15 22:10:00 +0100

    prepare chat react

--- src/App.jsx
+++ src/App.jsx
@@ -32,19 +32,28 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
-        <button>Send</button>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className='text-2xl font-bold my-2'>Chat React</h1>
+        <input
+          type="text"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 text-black w-full"
+        />
+
+        <ul className='h-80 overflow-y-auto'>
+          {messages.map((m, index) => {
+            return (
+              <li key={index} className={`my-2 p-2 table text-sm rounded-md ${m.from === 'me' ? 'bg-sky-700 ml-auto' : 'bg-black'}`}>
+                {m.from}: {m.body}
+              </li>
+            )
+          })}
+        </ul>
       </form>
 
-      {messages.map((m, index) => {
-        return (
-          <div key={index}>
-            <p>{m.from}: {m.body}</p>
-          </div>
-        )
-      })}
+
     </div>
   );
 };
